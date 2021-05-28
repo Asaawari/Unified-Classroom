73618ccb05aaf2c3fccec187c1cb3f81b236d84f
Sravas Melarkode <63852545+SupersonicCoder18@users.noreply.github.com>
2021-05-28 16:30:36 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Main from "./screens/Main";
+
+export default function App() {
+    return (
+      <App/>
+    )
+  }
---
+++ db.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBTZLewrMlZpz2Gl8GuLEOmvIGiLRw_oSY",
+    authDomain: "unifiedclassroom-455bd.firebaseapp.com",
+    projectId: "unifiedclassroom-455bd",
+    storageBucket: "unifiedclassroom-455bd.appspot.com",
+    messagingSenderId: "1010838417268",
+    appId: "1:1010838417268:web:e099cfde51802e0cbb0621"
+  };
