3868b729954bfef5023202705225c1d4c0393c6d
Asaawari <61871556+Asaawari@users.noreply.github.com>
2021-06-21 18:53:17 +0530

    Add files via upload

---
+++ Multiple-Choice-Quiz-JavaScript-master/config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBTZLewrMlZpz2Gl8GuLEOmvIGiLRw_oSY",
+    authDomain: "unifiedclassroom-455bd.firebaseapp.com",
+    projectId: "unifiedclassroom-455bd",
+    storageBucket: "unifiedclassroom-455bd.appspot.com",
+    messagingSenderId: "1010838417268",
+    appId: "1:1010838417268:web:e099cfde51802e0cbb0621"
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
